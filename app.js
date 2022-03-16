3d959810bd4a7a71598413bcb4c15345d48993eb
Byekibe <kibetpeter95@gmail.com>
2022-03-16 11:31:47 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h2>My first node server</h2>");
+});
+
+server.listen(5000, () => {
+  console.log("Starting server at port 5000");
+});
