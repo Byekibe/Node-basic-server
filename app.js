33bbe5db467cb568901114e6e232e296c21dc7cb
Byekibe <kibetpeter95@gmail.com>
2022-03-16 13:32:20 +0300

    Added a README.md file

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h2>My first node server</h2>");
+  res.end("<h2>My first node server!!</h2>");
 });
 
 server.listen(5000, () => {
-  console.log("Starting server at port 5000");
+  console.log("Starting server at port 5000!!!");
 });
+
+
+
+
